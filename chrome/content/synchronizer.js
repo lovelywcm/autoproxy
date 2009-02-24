090dc711ad797e06a31ab5b49343729fe1f7e874
lovelywcm <lovelywcm@gmail.com>
2009-02-25 04:48:16 +0800

    Subscription base64 encode support done.

--- chrome/content/synchronizer.js
+++ chrome/content/synchronizer.js
@@ -95,8 +95,16 @@ var synchronizer =
     let lines = text.split(/[\r\n]+/);
     if (!/\[AutoProxy\s+\d\.\d\.\d\]/i.test(lines[0]))
     {
-      this.setError(subscription, "synchronize_invalid_data");
-      return null;
+      //base64 --wrap=1
+      text = text.replace(/[\r\n]/g, '');
+      //base64 decode
+      text = windowMediator.getMostRecentWindow("").atob(text);
+      lines = text.split(/[\r\n]+/);
+      if (!/\[AutoProxy\s+\d\.\d\.\d\]/i.test(lines[0]))
+      {
+        this.setError(subscription, "synchronize_invalid_data");
+        return null;
+      }
     }
     let minVersion = RegExp.$1;
 

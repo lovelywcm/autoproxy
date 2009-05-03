2e8cf92ebe4272ac2fa844252e79b370be3cdf50
lovelywcm <lovelywcm@gmail.com>
2009-05-03 15:19:21 +0800

    abp 1727: Remove SeaMonkey chrome registration hacks

--- chrome/content/prefs.js
+++ chrome/content/prefs.js
@@ -100,12 +100,6 @@ var prefs = {
   },
 
   init: function() {
-
-    // Try to fix selected locale in Mozilla/SeaMonkey
-    strings = stringService.createBundle("chrome://autoproxy/locale/global.properties");
-    fixPackageLocale();
-    strings = stringService.createBundle("chrome://autoproxy/locale/global.properties");
-
     // Initialize prefs list
     var defaultBranch = prefService.getDefaultBranch(prefRoot);
     var defaultPrefs = defaultBranch.getChildList("", {});

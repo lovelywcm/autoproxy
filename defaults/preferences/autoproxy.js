5dda0a97ddef259db7c3d4b1dcb0abc924148336
lovelywcm <lovelywcm@gmail.com>
2009-11-07 17:00:34 +0800

    prefs: sort by name

--- defaults/preferences/autoproxy.js
+++ defaults/preferences/autoproxy.js
@@ -1,29 +1,31 @@
 // For description of these values see http://autoproxy.org/en/preferences
 
+pref("extensions.autoproxy.composer_default", 2);
 pref("extensions.autoproxy.currentVersion", "0.0");
-pref("extensions.autoproxy.showintoolbar", true);
-pref("extensions.autoproxy.showinstatusbar", false);
-pref("extensions.autoproxy.detachsidebar", false);
-pref("extensions.autoproxy.synchronizationinterval", 24);
-pref("extensions.autoproxy.defaulttoolbaraction", 2);
 pref("extensions.autoproxy.defaultstatusbaraction", 2);
-pref("extensions.autoproxy.sidebar_key", "Accel Shift O");
-pref("extensions.autoproxy.settings_key", "Accel Shift P");
+pref("extensions.autoproxy.defaulttoolbaraction", 2);
+pref("extensions.autoproxy.detachsidebar", false);
 pref("extensions.autoproxy.enable_key", "");
 pref("extensions.autoproxy.flash_scrolltoitem", true);
-pref("extensions.autoproxy.previewimages", true);
-pref("extensions.autoproxy.patternsfile", "autoproxy/patterns.ini");
-pref("extensions.autoproxy.patternsbackups", 5);
-pref("extensions.autoproxy.patternsbackupinterval", 24);
-pref("extensions.autoproxy.proxyableSchemes", "http https ftp");
 pref("extensions.autoproxy.hideimagemanager", true);
-pref("extensions.autoproxy.subscriptions_fallbackurl", "http://autoproxy.org/getSubscription?url=%s");
-pref("extensions.autoproxy.subscriptions_fallbackerrors", 5);
+pref("extensions.autoproxy.patternsbackupinterval", 24);
+pref("extensions.autoproxy.patternsbackups", 5);
+pref("extensions.autoproxy.patternsfile", "autoproxy/patterns.ini");
+pref("extensions.autoproxy.previewimages", true);
 pref("extensions.autoproxy.savestats", true);
-pref("extensions.autoproxy.composer_default", 2);
-pref("extensions.autoproxy.defaultProxy", "Tor;;9050;socks");
-pref("extensions.autoproxy.proxyMode", "auto");
+pref("extensions.autoproxy.settings_key", "Accel Shift P");
+pref("extensions.autoproxy.showinstatusbar", false);
+pref("extensions.autoproxy.showintoolbar", true);
+pref("extensions.autoproxy.sidebar_key", "Accel Shift O");
+pref("extensions.autoproxy.subscriptions_fallbackerrors", 5);
+pref("extensions.autoproxy.subscriptions_fallbackurl", "http://autoproxy.org/getSubscription?url=%s");
+pref("extensions.autoproxy.synchronizationinterval", 24);
+
 pref("extensions.autoproxy.customProxy", "");
+pref("extensions.autoproxy.defaultProxy", "Tor;;9050;socks");
 pref("extensions.autoproxy.knownProxy", "Free Gate;;8580;$GAppProxy;;8000;$GPass;;8000;$JAP;;4001;$Puff;;1984;$ssh -D;;7070;socks$Tor;;9050;socks$Wu Jie;;9666;$Your Freedom;;8080;");
+pref("extensions.autoproxy.proxyableSchemes", "http https ftp");
+pref("extensions.autoproxy.proxyMode", "auto");
+
 pref("extensions.autoproxy@autoproxy.org.description", "chrome://autoproxy/locale/global.properties");
 
